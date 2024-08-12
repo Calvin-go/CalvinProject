de7f87dbf244f59fa805ac66458795730062feae
Calvin <calvin.go.808@gmail.com>
2024-08-12 11:05:56 +0800

    Create func_py_calculate_circle_circumference.py

---
+++ Code/func_py_calculate_circle_circumference.py
@@ -0,0 +1,7 @@
+# func_py_calculate_circle_circumference.py
+import math
+
+def func_py_calculate_circle_circumference(radius):
+    return 2 * math.pi * radius
+
+print(func_py_calculate_circle_circumference(7))
