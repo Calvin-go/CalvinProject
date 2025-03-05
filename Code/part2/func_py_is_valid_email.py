2fb4f37ab4ad25739e0091adff2f9a7df2c164b5
Calvin <calvin.go.808@gmail.com>
2025-03-05 11:24:42 +0800

    Create func_py_is_valid_email.py

---
+++ Code/part2/func_py_is_valid_email.py
@@ -0,0 +1,6 @@
+# func_py_is_valid_email.py
+import re
+
+def func_py_is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return bool(re.match(pattern, email))
